d0a60cf27993e4f938f142ac680fda34160345cb
James <jamesliverman77@gmail.com>
2020-10-24 22:31:35 +0430

    Add files via upload

---
+++ week1/liverman-assignment1.4.js
@@ -0,0 +1,44 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: James Liverman
+; Date: 23 October 2020
+; Modified By: James Liverman
+; Description: This program demonstrates the
+; use of JavaScript types, values, and 
+; and variables in an application.
+;===========================================
+*/ 
+//start program
+
+/*Expected Output
+
+Create a variable named firstName and assign it a value of "your actual first name"
+Create a variable named lastName and assign it a value of "your actual last name"
+Create a variable named age and assign it a value of "your actual age"
+Create a variable named street and assign it a value of "some random street address"
+Create a variable named city and assign it a value of "some random city name"
+Create a variable named zipCode and assign it a value of "some random zip code"
+Use the console.log function to display the contents of each variable
+Run and test the program and make sure your output matches what I have 
+When you are happy with the output stage, commit, and push your work to GitHub
+Starter Code: GitHub repo 
+https://www.w3schools.com/jsref/met_console_log.asp   */
+//variable and declaration goes here
+var firstName ="James";
+var lastName ="Liverman";
+var age = 42;
+var street = "some random street address";
+var city = "some random city name";
+var zipCode = "some random zip code";
+//output
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
+
+
+//end program
+
---
+++ week1/liverman-assignment1.5.js
@@ -0,0 +1,74 @@
+/*
+============================================
+; Title: Assignment 1.5
+; Author: James Liverman
+; Date: 24 October 2020
+; Modified By: James Liverman
+; Description: This program demonstrates the
+; use of JavaScript types, values, and 
+; and variables in an application.
+;===========================================
+*/ 
+//start program
+
+/*Expected Output
+
+Create a variable named firstName1 and assign it a value of "James"
+Create a variable named firstName2 and assign it a value of "Jim"
+Create a variable named firstName3 and assign it a value of "Jake"
+Create a variable named lastName1 and assign it a value of "Liverman"
+Create a variable named lastName2 and assign it a value of "Kirk"
+Create a variable named lastName3 and assign it a value of "Smith"
+Create a variable named address1 and assign it a value of "6333 Nowhere drive, SD CA 90101"
+Create a variable named address2 and assign it a value of "6000 Nowhere drive, SD CA 90101"
+Create a variable named address3 and assign it a value of "6200 Nowhere drive, SD CA 90101"
+Create a variable named payRate1 and assign it a value of 40.00
+Create a variable named payRate2 and assign it a value of 45.00
+Create a variable named payRate3 and assign it a value of 55.00
+Create a variable named hireDate1 and assign it a value of The 1st day of the 1st month of 2010
+Create a variable named hireDate2 and assign it a value of The 1st day of the 1st month of 2011
+Create a variable named hireDate3 and assign it a value of The 1st day of the 2nd month of 2010
+
+Use the console.log function to display the contents of each variable
+Run and test the program and make sure your output matches what I have 
+When you are happy with the output stage, commit, and push your work to GitHub
+Starter Code: GitHub repo 
+https://www.w3schools.com/jsref/met_console_log.asp   */
+//variable and declaration goes here
+
+var firstName1 = "James";
+var firstName2 = "Jim";
+var firstName3 = "Jake";
+var lastName1 = "Liverman";
+var lastName2 = "Kirk";
+var lastName3 = "Smith";
+var address1 = "6333 Nowhere drive, SD CA 90101";
+var address2 = "6000 Nowhere drive, SD CA 90101";
+var address3 = "6200 Nowhere drive, SD CA 90101";
+var payRate1 = 40.00;
+var payRate2 = 45.00;
+var payRate3 = 55.00;
+var hireDate1 = new Date(2010, 0, 1); // The 1st day of the 1st month of 2010
+var hireDate2 = new Date(2011, 0, 1); // The 1st day of the 1st month of 2011
+var hireDate3 = new Date(2012, 0, 2); // The 1st day of the 2nd month of 2010
+//output
+console.log(firstName1);
+console.log(firstName2);
+console.log(firstName3);
+console.log(lastName1);
+console.log(lastName2);
+console.log(lastName3);
+console.log(address1);
+console.log(address2);
+console.log(address3);
+console.log(payRate1);
+console.log(payRate2);
+console.log(payRate3);
+console.log(hireDate1);
+console.log(hireDate2);
+console.log(hireDate3);
+
+
+
+//end program
+
