62f97362dd66547f79339940b85f20cb5cc0376c
James <jamesliverman77@gmail.com>
2020-10-30 20:49:18 +0430

    Add files via upload
    files changed per instructions

---
+++ week2/liverman-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  liveran-header-test.js
+; Author: Professor Krasso
+; Date:   30 October 2020
+; Modified by: James Liverman
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('./header.js');
+
+console.log(header.display("James", "Liverman", "Week 2 Exercise 2.2"));
