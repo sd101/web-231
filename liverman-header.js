4ba484c00c8e3892a94fffaf895d8bb67d323941
James <jamesliverman77@gmail.com>
2020-10-30 20:21:54 +0430

    Add files via upload
    header file created and added to main library

---
+++ liverman-header.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso
+; Date:   30 October 2020
+; Modified By: James Liverman
+; Description: Displays a formatted header. Updated line 5 (Date)
+and 6 (Modified By). This file is for creating a personal header file.
+;===========================================
+*/
+
+const header = require('./header.js');
+
+console.log(header.display("Robert", "Frost", "Week 2"));
